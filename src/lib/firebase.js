30f16e01e741a9ac90bf705a8d89186eceed7b51
Sudeep Mukul <sudeepmukul@gmail.com>
2026-01-05 02:36:17 +0530

    Security: Remove hardcoded API keys and use environment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-p8GEAN1vomHlDA1GreKArktKQbiLCtI",
-    authDomain: "zencyflow.firebaseapp.com",
-    projectId: "zencyflow",
-    storageBucket: "zencyflow.firebasestorage.app",
-    messagingSenderId: "688088421850",
-    appId: "1:688088421850:web:d1fb47eb831aaf706adce3",
-    measurementId: "G-C6CCVVC8E8"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
